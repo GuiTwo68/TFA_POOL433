01d7a558ae4afdba1755e463bcf2760b13b2a391
GuiTwo68 <102789456+GuiTwo68@users.noreply.github.com>
2022-04-01 09:23:31 +0200

    Add files via upload

---
+++ src/TFA_POOL433.cpp
@@ -0,0 +1,193 @@
+#include "TFA_POOL433.h"
+
+#define FILTER_MIN 	950
+#define FILTER_MAX 	9500
+#define MEDIUM_LEN 	3000
+#define START_LEN 	9000
+#define STOP_LEN 	2000
+
+#define REPEAT_DELAY 3000 //ms
+
+#define dbg(s) Serial.println(s)
+#define dbgs(s) Serial.print(s)
+//define dbg(s)
+
+const int TFA_POOL433::_BUFF_SIZE = 36;
+
+volatile bool TFA_POOL433::_avail = false;
+volatile byte TFA_POOL433::_buff[TFA_POOL433::_BUFF_SIZE];
+volatile byte TFA_POOL433::_buffEnd = 0;
+volatile bool TFA_POOL433::_STARTb = 0;
+
+
+
+unsigned long TFA_POOL433::_lastPackageArrived;
+byte TFA_POOL433::_lastBuff[TFA_POOL433::_BUFF_SIZE];
+byte TFA_POOL433::_pin = 0;
+
+TFA_POOL433::TFA_POOL433() {
+}
+
+void TFA_POOL433::start(int pin) {
+	_pin = pin;
+	pinMode(_pin, INPUT);
+	// for (int i = 0; i < _BUFF_SIZE; i++) {
+		// _buff[i] = 8;
+		// _lastBuff[i] = 8;
+	// }
+	_lastPackageArrived = 0;
+	_avail = false;
+	_buffEnd=0;
+	attachInterrupt(digitalPinToInterrupt(_pin), _handler, FALLING);
+	dbg("tfa started");
+}
+
+void TFA_POOL433::stop(){
+	detachInterrupt(digitalPinToInterrupt(_pin));
+}
+
+bool TFA_POOL433::isDataAvailable(){
+	return _avail;
+}
+
+void TFA_POOL433::_handler() {
+	static unsigned long lastMs = 0, currMs,diffMs;
+	currMs = micros();
+	diffMs = currMs - lastMs;
+	lastMs = currMs;
+
+	if (diffMs > FILTER_MIN && diffMs < FILTER_MAX)  //Filter out the too long and too short pulses
+	{
+		if (!_avail)  //avail means data available for processing
+		{
+			if (diffMs > START_LEN)  // INIT/STOP pulse
+			{
+			_STARTb = 1;
+			//dbgs("START");
+			_buffEnd = 0;
+			}
+			else if ( _STARTb == 1)
+			{
+				if (_buffEnd > _BUFF_SIZE)
+				{
+					_buffEnd = 0;
+					_avail = false;
+					_STARTb = 0;
+					//dbg("Buffer owerflow1");
+				} else if (diffMs < STOP_LEN)  // STOP pulse short
+				{
+					if (_buffEnd == _BUFF_SIZE )
+					{
+						//dbg("STOP");// 
+						_avail = true;
+						_STARTb = 0;
+						//dbg("S buffEnd:" + String(_buffEnd));
+					}
+					else if (_buffEnd > _BUFF_SIZE) // useful ?
+						{
+						_buffEnd = 0;
+						_avail = false;
+						_STARTb = 0;
+						//dbg("Buffer owerflow2");
+						}
+					
+				} else 
+				{
+					if (_buffEnd < _BUFF_SIZE)   //buffer is not full yet
+					{
+						if (diffMs < MEDIUM_LEN)  //0
+						{
+							_buff[_buffEnd++] = 0;
+						//	dbgs("0");
+						} else { //1
+							_buff[_buffEnd++] = 1;
+						//	dbgs("1");
+						}
+					}
+				}
+			}
+		}	
+	}
+		
+	
+}
+
+bool TFA_POOL433::_isRepeat() {
+	bool result = false;
+	for(int i=0;i<_buffEnd;i++){
+		if (_buff[i]!=_lastBuff[i]){
+			for (int j=0;j<_buffEnd;j++){
+				_lastBuff[j] = _buff[j];
+			}
+			_lastPackageArrived = millis();
+			return false;
+		}
+	}
+	result = (millis()-_lastPackageArrived < REPEAT_DELAY);
+	_lastPackageArrived = millis();
+	return result;
+}
+
+void TFA_POOL433::getData(byte &id, byte &channel, int &temperature, bool &battery){
+
+	int temp1 = 0;
+	
+
+
+
+	for(int i=0;i<_buffEnd;i++) {
+		dbgs(_buff[i]);
+	}
+	dbgs("\n");
+	id = _binToDecRev(_buff, 0, 3);
+	channel = _binToDecRev(_buff, 14, 15)+1;
+	temp1 = _binToDecRev(_buff, 16, 27);
+
+
+
+
+//fixed id: 0 till 3
+//rolling code: 4 till 13
+
+//Channel: 14 till 15
+//Temperature: 16 till 27
+//Battery
+
+  
+
+	// Convert from Temperature
+	temperature = temp1*10;//(((((temp1) * 100) - 14800 - 3200) * 5) / 9);
+
+
+
+	battery = 0;//_binToDecRev(_buff, 29, 30) != 1;
+	_STARTb = 0;
+	_avail = false;
+	_buffEnd = 0;
+	
+}
+
+tfaResult TFA_POOL433::getData(){
+	tfaResult result;
+	getData(result.id, result.channel, result.temperature, result.battery);
+	_avail = false;
+	return result;
+}
+
+int TFA_POOL433::_binToDecRev(volatile byte *binary, int s, int e) {
+	int result = 0;
+	unsigned int mask = 1;
+	for(; e > 0 && s<=e; mask <<= 1)
+	if(binary[e--] != 0)
+		result |= mask;
+	return result;
+}
+
+int TFA_POOL433::_binToDec(volatile byte *binary, int s, int e) {
+	unsigned int mask = 1;
+	int result = 0;
+	for(; s<=e; mask <<= 1)
+	if(binary[s++] != 0)
+		result |= mask;
+	return result;
+}
---
+++ src/TFA_POOL433.h
@@ -0,0 +1,61 @@
+/*
+433 MHz wether station receiver library for so called TFA
+
+Pulse should look like this:
+     __   0   __     1     __    START/STOP    __
+   _|  |_____|  |_________|  |________________|  |_
+
+Based on my measurement pulse lengths are the following:
+0: 1500 - 2500 us
+1: 3500 - 4500 us
+S: 7500 - 9000 us
+
+Pulses shorter than 1500 and longer than 9000 are skipped.
+Before the first data bit arrived usually 8-16 "S" pulsey are coming.
+Senders are usually repeats the message 3-6 times. Between repeats there are 2 "S" pulses.
+
+*/
+#ifndef TFA_POOL433_h
+#define TFA_POOL433_h
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+typedef struct{
+	byte id;
+	byte channel;
+	int temperature;
+	bool battery;
+} tfaResult;
+
+class TFA_POOL433{
+	public:
+		TFA_POOL433();
+		void start(int pin);
+		void stop();
+		bool isDataAvailable();
+		void getData(byte &id, byte &channel, int &temperature, bool &battery);
+		tfaResult getData();
+		
+	private:
+		const static int _BUFF_SIZE;
+
+		volatile static bool _avail;
+		volatile static byte _buff[];
+		volatile static byte _buffEnd;
+		volatile static bool _STARTb;
+
+		static unsigned long _lastPackageArrived;
+		static byte _lastBuff[];
+		static byte _pin;
+
+		static void _handler();
+		static bool _isRepeat();
+		int _binToDecRev(volatile byte *binary, int s, int e);
+		int _binToDec(volatile byte *binary, int s, int e);
+};
+
+#endif
